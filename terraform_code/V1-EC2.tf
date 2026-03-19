10d20fcc0b84acc8604c3e65133755a410d74622
Zelia Desouki <zeoncloud24@gmail.com>
2026-03-19 21:23:14 +0200

    upadating file

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -6,4 +6,27 @@ resource "aws_instance" "demo-server" {
   ami = "ami-02dfbd4ff395f2a1b"
   instance_type = "t3.micro"
   key_name = "dpp"
+  security_groups = [ "demo-sg" ]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name   = "demo-sg"
+  description = "SSH Access"
+  tags = {
+    Name = "ssh-prot"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.demo-sg.id
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  cidr_ipv4         = ["0.0.0.0/0"]
+}
+
+resource "aws_vpc_security_group_egress_rule" "all" {
+  security_group_id = aws_security_group.demo-sg.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = ["0.0.0.0/0"]
 }
